914b5ce1d7aaceb0cfeea9de17614488d8e198a5
Bildad Ronoh <Bildadronoh@gmail.com>
2024-04-24 22:55:46 +0300

    add newcollection list to admin

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './NewCollections.css'
-import new_collections from '../Assests/new_collections'
+
 import Item from '../Item/Item'
 
 const NewCollections = () => {
 
   const [new_collection,setNew_collection] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/newcollections')
+    .then((response)=>response.json())
+    .then((data)=>setNew_collection(data));
+  },[])
+
   return (
     <div className='new-collections'>
         <h1> NEW COLLECTIONS</h1>
         <hr/>
         <div className="collections">
-            {new_collections.map((item,i)=>{
+            {new_collection.map((item,i)=>{
                 return (
                     < Item
                       key={i}
